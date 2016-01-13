86a5d260bc79513077f214252e99c332299d576c
JoseD92 <jose_daniel_d@hotmail.com>
2016-01-12 21:22:40 -0430

    monedas y prueba de player

---
+++ Assets/Coin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+    public int rotationSpeed;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(45*Time.deltaTime*rotationSpeed,0,0);
+	}
+}
---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+
+    private int dinero;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+        dinero = 100;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = (transform.position + new Vector3(-1, 0, 0)*Time.deltaTime*speed) ; 
+	}
+}
