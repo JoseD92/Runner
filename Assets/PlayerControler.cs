3d4424ef44de4e80b08ca8b3e506c46774251d1d
JoseD92 <jose_daniel_d@hotmail.com>
2016-01-14 15:22:03 -0430

    mejorado plano a texturas dinamicas

--- Assets/PlayerControler.cs
+++ Assets/PlayerControler.cs
@@ -9,11 +9,15 @@ public class PlayerControler : MonoBehaviour {
     private int pos;
     public Text countText;
     public Animator anim;
+    private Material[] m;
+    public GameObject plano;
 
 	// Use this for initialization
 	void Start () {
         dinero = 100;
         pos = 2;
+        m = plano.GetComponent<Renderer>().materials;
+        m[1].color = new Color(0,255,0);
 	}
 	
 	// Update is called once per frame
@@ -27,13 +31,17 @@ public class PlayerControler : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.LeftArrow) && pos != 1)
             {
                 transform.position = transform.position + new Vector3(0, 0, -1);
+                m[pos - 1].color = new Color(0.4980392f , 0.4980392f , 0.4980392f , 1);
                 pos -= 1;
+                m[pos - 1].color = new Color(0, 255, 0);
             }
 
             if (Input.GetKeyDown(KeyCode.RightArrow) && pos != 3)
             {
                 transform.position = transform.position + new Vector3(0, 0, 1);
+                m[pos - 1].color = new Color(0.4980392f , 0.4980392f , 0.4980392f , 1);
                 pos += 1;
+                m[pos - 1].color = new Color(0, 255, 0);
             }
 
             if (Input.GetKeyDown(KeyCode.Space))
